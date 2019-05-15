73f740ff841621cd2fbf40a0315f53768c68cecc
Mads Smed <mads.th@hotmail.com>
2019-05-15 16:47:29 +0200

    Added Data Annotations

--- HumanRisksAssignment/Models/RiskAssessment.cs
+++ HumanRisksAssignment/Models/RiskAssessment.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace HumanRisksAssignment.Models
 {
     class RiskAssessment
     {
-        public string Id { get; set; }
+        [Key]
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public List<Threat> Threats;
+
+        public List<Threat> Threats { get; }
     }
 }
--- HumanRisksAssignment/Models/Threat.cs
+++ HumanRisksAssignment/Models/Threat.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace HumanRisksAssignment.Models
 {
     class Threat
     {
-        public Guid Id { get; } = Guid.NewGuid();
+        [Key]
+        public Guid Id { get; }
         public string Title { get; set; }
+        [Range(0, 2, ErrorMessage = "No such Threat level")]
         public int Level { get; set; }
+        [ForeignKey("RiskAssessment")]
         public string RiskAssessmentId { get; }
-
-        public Threat()
-        {
-
-        }
     }
 }

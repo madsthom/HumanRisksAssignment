8840883298a5e7aaca7c1c5aa7d86df00821f3da
Mads Smed <mads.th@hotmail.com>
2019-05-15 16:22:38 +0200

    Added Models

---
+++ HumanRisksAssignment/Models/RiskAssessment.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanRisksAssignment.Models
+{
+    class RiskAssessment
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public List<Threat> Threats;
+    }
+}
---
+++ HumanRisksAssignment/Models/Threat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanRisksAssignment.Models
+{
+    class Threat
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public int Level { get; set; }
+        public string RiskAssessmentId { get; }
+
+        public Threat()
+        {
+
+        }
+    }
+}
